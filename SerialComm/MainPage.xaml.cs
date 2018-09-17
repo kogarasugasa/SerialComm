a3624ffb1398a080f9ece5ea64f7779e9e7a3859
Hayato Kobayashi <wahuku.miyatuguti_35@hotmail.co.jp>
2018-09-17 12:53:10 +0900

    Initialize

--- SerialComm/MainPage.xaml.cs
+++ SerialComm/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Devices.SerialCommunication;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
@@ -25,6 +26,31 @@ namespace SerialComm
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        public async void Test()
+        {
+            string portName = "COM5";
+            string aqs = SerialDevice.GetDeviceSelector(portName);
+
+            var myDevices = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(aqs, null);
+
+            if(myDevices.Count == 0)
+            {
+                return;
+            }
+
+            var device = await SerialDevice.FromIdAsync(myDevices[0].Id);
+            device.BaudRate = 9600;
+            device.DataBits = 8;
+            device.StopBits = SerialStopBitCount.One;
+            device.Parity = SerialParity.None;
+            device.Handshake = SerialHandshake.None;
+            device.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+            device.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+
+
+
         }
     }
 }
